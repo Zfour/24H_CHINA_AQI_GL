7133b4fff601be870f38b2d37624dce2cb0492be
Zfour <499984532@qq.com>
2021-01-23 14:26:00 +0800

    上传了文件

---
+++ js/bmap.min.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html><head><meta charset="utf-8"><meta name="robots" content="noindex" /><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1" user-scalable="no"><style>html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
+.main {
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-weight: normal;
+    font-family: Sans-serif;
+}
+.content {
+    transform: translate(-50%, -50%);
+}</style></head><body><div class="main"><div class="content"><div><span>ECharts 正在 Apache 开源基金会孵化中，因此域名已不再使用，请访问 &nbsp;</span><a id="a1" href="https://echarts.apache.org/zh/index.html">echarts.apache.org</a></div><div><span>ECharts is now under incubation at the Apache Software Foundation. So this domain name is no longer in use. Visit </span><a id="a2" href="https://echarts.apache.org/zh/index.html">echarts.apache.org</a><span> please</span></div></div></div><script type="text/javascript">document.getElementById('a1').setAttribute('href', 'https://echarts.apache.org'+((!location.pathname||location.pathname==='/')?'/zh/index.html':location.pathname+(!location.search?'':location.search)+(!location.hash?'':location.hash)));document.getElementById('a2').setAttribute('href', document.getElementById('a1').getAttribute('href'));</script></body>
